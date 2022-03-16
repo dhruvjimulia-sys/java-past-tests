6dd9599c104171ecfd375c67bc409d0aa2313c5f
Dhruv Jimulia <dj321@ic.ac.uk>
2022-03-16 13:58:00 +0000

    Completed Section A Video Game

--- videogame_and_tictactoe/SectionA/src/videogame/Magician.java
+++ videogame_and_tictactoe/SectionA/src/videogame/Magician.java
@@ -1,3 +1,31 @@
 package videogame;
 
-// TODO: complete this class as part of Section A Question 3
+public class Magician extends Entity implements SpellCaster {
+
+  public Magician(String name, int lifePoints) {
+    super(name, lifePoints);
+  }
+
+  @Override
+  public int getStrength() {
+    return lifePoints * 2;
+  }
+
+  @Override
+  protected int propagateDamage(int damageAmount) {
+    assert damageAmount >= 0;
+    final int effectiveDamageAmount = Math.min(damageAmount, lifePoints);
+    lifePoints -= effectiveDamageAmount;
+    return effectiveDamageAmount;
+  }
+
+  @Override
+  public int minimumStrikeToDestroy() {
+    return lifePoints;
+  }
+
+  @Override
+  public String toString() {
+    return name + "(" + lifePoints + ")";
+  }
+}
--- videogame_and_tictactoe/SectionA/src/videogame/TransportUnit.java
+++ videogame_and_tictactoe/SectionA/src/videogame/TransportUnit.java
@@ -1,3 +1,47 @@
 package videogame;
 
-// TODO: complete this class as part of Section A Question 4
+import java.util.HashSet;
+import java.util.Set;
+
+public class TransportUnit extends Entity {
+  Set<Entity> entities;
+
+  public TransportUnit(String name, int lifePoints) {
+    super(name, lifePoints);
+    entities = new HashSet<>();
+  }
+
+  @Override
+  protected int propagateDamage(int damageAmount) {
+    assert damageAmount >= 0;
+    final int effectiveDamageAmount = Math.min(damageAmount, lifePoints);
+    lifePoints -= effectiveDamageAmount;
+    return effectiveDamageAmount
+        + entities.stream().map(e -> e.propagateDamage(damageAmount / 2)).reduce(0, Integer::sum);
+  }
+
+  @Override
+  public int minimumStrikeToDestroy() {
+    return Math.max(
+        lifePoints,
+        entities.stream()
+            .map(Entity::minimumStrikeToDestroy)
+            .map(i -> i * 2)
+            .reduce(Integer.MIN_VALUE, Math::max));
+  }
+
+  public void add(Entity entity) {
+    entities.add(entity);
+  }
+
+  @Override
+  public String toString() {
+    return name
+        + "("
+        + lifePoints
+        + ") "
+        + "transporting: ["
+        + entities.stream().map(Object::toString).reduce((a, b) -> a + ", " + b).orElse("")
+        + "]";
+  }
+}
